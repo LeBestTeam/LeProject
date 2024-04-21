ef4ad7612464c121d8df00f709cf2baddb79a311
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-04-22 00:51:15 +0300

    Example of affine fractal

--- code/main.py
+++ code/main.py
@@ -15,6 +15,9 @@ class App:
     def create_fractal(self, name, *args):
         """
         Checks if fractal name given exists and creates it
+
+        # Available fractals:
+        `lfractal`, `afractal`, `mfractal`
         """
         if name in self.fractals:
             return self.fractals[name](*args).build()
@@ -25,4 +28,11 @@ class App:
 if __name__ == "__main__":
     app = App()
     app.create_fractal("lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 5, 0, np.pi/2)
-    app.create_fractal("mfractal")
+    app.create_fractal("afractal", [
+        [1.0, -0.1],
+        [0.2, -1.0],
+        [-0.3, 0.4],
+        [0.7, 0.2],
+        [0.5, -0.4],
+        [-0.2, 0.5]
+])
