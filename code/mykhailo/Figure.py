3e43375f8685921be330367c859decfcba3d4e3b
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-10 19:10:29 +0300

    Create Figure.py

---
+++ code/mykhailo/Figure.py
@@ -0,0 +1,46 @@
+class Figure:
+    def __init__(self):
+        pass
+
+    def generate_points(self):
+        pass
+
+
+class FigureBuilder:
+    def __init__(self, *args):
+        pass
+
+    def check_args(self):
+        """
+        default functionality example
+        """
+        check_success = True
+        if check_success:
+            self.check_succes = True
+            return 1
+        else:
+            import warnings
+            warnings.warn("Check failed")
+            self.check_succes = False
+            return 0
+
+    def get_figure(self):
+        """
+        default functionality example
+        """
+
+        if self.check_succes is None:
+            import warnings
+            warnings.warn("Check has not been completed yet")
+            return 0
+        if self.check_succes:
+            figure = Figure()
+            def generate_points():
+                return [["x"], ["y"]]
+
+            figure.generate_points = generate_points
+            return figure
+        else:
+            import warnings
+            warnings.warn("Chack has failed")
+            return 0
