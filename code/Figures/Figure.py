49c38924c2691b514152f149e395dc5568abbc2a
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-11 16:34:33 +0300

    Move to its own directory

---
+++ code/Figures/Figure.py
@@ -0,0 +1,9 @@
+class Figure:
+    def __init__(self, *args):
+        pass
+
+    def check_args(self, *args):
+        pass
+
+    def generate_points(self):
+        pass
---
+++ code/Figures/LsystemFractal.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+class LsystemFractal:
+    """
+    Lsystem implementation of fractals (see compgraph Lab3)
+
+    Now there is no Animation and only radians
+    """
+
+    def __init__(self, axiom: str, rules: dict, max_iterations: int, fi: float, dfi: float, *args):
+        """
+        Initiates Lsystem fractal with given parameters but before checks if parameters are correct
+
+        # Parameters:
+        axiom: string (starting L-axiom)
+        rules: dict (rules for how to change each letter (not specific symbol) in iteration)
+        max_iterations: int (How many iterations)
+        fi: float (starting angular) (now only radians)
+        dfi: float (angular velocity) (now only radians)
+        """
+
+        if args != ():
+            raise ValueError(f"Wrong number of arguments, {args} excess")
+        self.list_to_check = [str, dict, int, float, float]  # Change if count of arguments changes
+        self.check_args(axiom, rules, max_iterations, fi, dfi)
+
+        self.axiom = axiom
+        self.rules = rules
+        self.max_iterations = max_iterations
+        self.fi = fi
+        self.dfi = dfi
+
+    def check_args(self, *args):
+        """
+        Checks if parameters are correct
+
+        if not - raises ValueError with appropriate message
+        """
+        for argument_index in range(len(args)):
+            if type(args[argument_index]) is not self.list_to_check[argument_index]:
+                raise ValueError(f"Wrong argument {args[argument_index]}, which is {type(args[argument_index])} type, expected {self.list_to_check[argument_index]} type")
+
+    def generate_points(self):
+        """
+        Generates more and more of Lsystem fractal on each iteration
+
+        # Updates:
+        self.axiom each iteration
+
+        Calculates all coordinates of Lsystem fractal
+
+        # Returns:
+        (N+1 shape, N+1 shape) arrays of x and y coordinates
+        """
+        for iteration in range(self.max_iterations):
+            new_axiom = ''
+            for word_place in range(len(self.axiom)):
+                if self.axiom[word_place] in self.rules.keys():
+                    new_axiom += self.rules[self.axiom[word_place]]
+                else:
+                    new_axiom += self.axiom[word_place]
+            self.axiom = new_axiom
+
+        N = len(self.axiom)
+        L = 2
+        x = np.zeros(N+1)
+        y = np.zeros(N+1)
+        for i in range(N):
+            x[i+1] = x[i]
+            y[i+1] = y[i]
+            if self.axiom[i] == 'F':
+                x[i+1] += L*np.cos(self.fi)
+                y[i+1] += L*np.sin(self.fi)
+            elif self.axiom[i] == '+':
+                self.fi += self.dfi
+            elif self.axiom[i] == '-':
+                self.fi -= self.dfi
+        return x, y
