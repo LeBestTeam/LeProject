5bae98482eef1a6a9821d6921ec41c0f5c02b6a6
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-14 16:09:17 +0300

    Added new fractal

--- code/App.py
+++ code/App.py
@@ -27,21 +27,20 @@ class App:
 
 
 app = App()
-# app.create_fractal("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5)
-# app.create_fractal("Afractal", [
-#         [1.0, -0.1],
-#         [0.2, -1.0],
-#         [-0.3, 0.4],
-#         [0.7, 0.2],
-#         [0.5, -0.4],
-#         [-0.2, 0.5]
-# ], it=4*10**4)
+app.create_fractal("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5)
+app.create_fractal("Afractal", [
+        [1.0, -0.1],
+        [0.2, -1.0],
+        [-0.3, 0.4],
+        [0.7, 0.2],
+        [0.5, -0.4],
+        [-0.2, 0.5]
+], it=4*10**4)
 
-# app.create_fractal("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=2, is_matrix=True)
+app.create_fractal("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=2, is_matrix=True)
 
-# app.create_fractal("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
-# app.create_fractal("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
+app.create_fractal("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
+app.create_fractal("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
 
-# app.create_fractal("BrownianTree", False, it=30, is_random_matrix=True, animation_need=True)
-
-app.create_fractal("BrownianMotion", 30, it=30, is_matrix=True)
+app.create_fractal("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
+app.create_fractal("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
