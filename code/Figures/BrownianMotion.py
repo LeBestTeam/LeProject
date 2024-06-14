3f7ee26a4000328f0e681cfe72d8932e337b90b2
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-14 16:08:27 +0300

    Added new fractal

--- code/Figures/BrownianMotion.py
+++ code/Figures/BrownianMotion.py
@@ -1,45 +1,73 @@
 import numpy as np
-import numba
+import matplotlib.pyplot as plt
 
 
 class BrownianMotion:
-    def __init__(self, size,*args):
+    def __init__(self, size: int, *args):
+        if args != ():
+            raise ValueError(f"Wrong number of arguments, {args} excess")
+        self.list_to_check = [int]
+        self.check_args(size)
+
         self.size = size
 
     def check_args(self, *args):
-        pass
+        """
+        Checks if parameters are correct
+
+        if not - raises ValueError with appropriate message
+        """
+        for argument_index in range(len(args)):
+            if type(args[argument_index]) is not self.list_to_check[argument_index]:
+                raise ValueError(f"Wrong argument {args[argument_index]}, which is {type(args[argument_index])} type, expected {self.list_to_check[argument_index]} type")
 
     def generate_points(self, iteration):
-        
-        @numba.njit()
-        def checkAround(x, y, squareSize, matrix):
+        """
+        Generates dot of brownian motion on each iteration
+
+        # Returns:
+        (size, size) matrix that should be displayed as image (plt.imshow)
+        """
+        def checkAround(x, y, size_limit):
             """
             Checks if there is friend or exit from circle around point (x, y), if not, chooses random way to go
 
             # Returns:
             x - x coordinate,
             y - y coordinate,
-            friend_found - if there is dot nearby,
-            edge_near - if there is edge nearny,
-            exit_from_circle - if it exited from circle
             """
-            friend_found = False
-            exit_from_circle = False
-            edge_near = False
+            coefs = [0, 1, 2, 3]
+            if x - 1 < 0:
+                coefs.remove(0)
+            if x + 1 > size_limit - 1:
+                coefs.remove(1)
+            if y - 1 < 0:
+                coefs.remove(2)
+            if y + 1 > size_limit - 1:
+                coefs.remove(3)
+            array = np.array([0, 1, 2, 3])
+            variant = np.random.choice(array[coefs], 1)
+            x, y = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)][variant[0]]
 
-            if (x + 1) > squareSize - 1 or (x - 1) < 1 or (y + 1) > squareSize - 1 or (y - 1) < 1:
-                edge_near = True
+            return x, y
 
-                variant = np.random.choice(np.array([0, 1, 2, 3]), 1)
-                x, y = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)][variant[0]]
-
-            return x, y, friend_found, edge_near, exit_from_circle
-        
         matrix = np.zeros((self.size, self.size))
+        matrix_for_animation = []
+
         np.random.seed()
         x = np.random.randint(0, self.size)
         y = np.random.randint(0, self.size)
-        for i in range(iteration):
-            
 
-        return matrix
+        for i in range(iteration):
+            matrix[x, y] = 1
+            if i in np.arange(0, iteration, self.size//3):
+                matrix_for_animation.append(matrix.copy())
+            x, y = checkAround(x, y, self.size)
+
+        matrix_for_animation.append(matrix.copy())
+        return matrix_for_animation
+
+
+if __name__ == "__main__":
+    plt.imshow(BrownianMotion(100).generate_points(10000)[-1])
+    plt.show()
