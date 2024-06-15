56b93031b8dae921b4f99b64b6f616c067714597
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-15 18:29:26 +0300

    Added animation to all examples

--- code/App.py
+++ code/App.py
@@ -28,7 +28,7 @@ class App:
 
 
 app = App()
-app.create_fractal("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5)
+app.create_fractal("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
 app.create_fractal("Afractal", [
         [1.0, -0.1],
         [0.2, -1.0],
@@ -36,9 +36,9 @@ app.create_fractal("Afractal", [
         [0.7, 0.2],
         [0.5, -0.4],
         [-0.2, 0.5]
-], it=4*10**4)
+], it=4*10**4, animation_need=True, multi=200)
 
-app.create_fractal("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=2, is_matrix=True)
+app.create_fractal("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
 
 app.create_fractal("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
 app.create_fractal("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
@@ -49,4 +49,4 @@ app.create_fractal("NangularFractal", np.array([
     [0, 0],
     [1, 5],
     [2, -1],
-]), it=4*10**4)
+]), it=4*10**4, animation_need=True, multi=200)
