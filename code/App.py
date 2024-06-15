8946831e251c217d72a425a419a1171f85f564be
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-15 23:11:31 +0300

    Rename create_fractal to create_figure

--- code/App.py
+++ code/App.py
@@ -6,7 +6,7 @@ import numpy as np
 
 class App:
     def __init__(self):
-        self.fractals_class = {
+        self.figures = {
             # Iteration Fractals
             "Lfractal": LsystemFractal.LsystemFractal,
             "Afractal": AffineFractal.AffineFractal,
@@ -20,16 +20,16 @@ class App:
             "NangularFractal": NangularFractal.NangularFractal
         }
 
-    def create_fractal(self, name, *args, **kwargs):
-        if name in self.fractals_class:
-            FigureDirector().build(self.fractals_class[name](*args), **kwargs)
+    def create_figure(self, name, *args, **kwargs):
+        if name in self.figures:
+            FigureDirector().build(self.figures[name](*args), **kwargs)
         else:
             raise ValueError("Wrong fractal name")
 
 
 app = App()
-app.create_fractal("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
-app.create_fractal("Afractal", [
+app.create_figure("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
+app.create_figure("Afractal", [
         [1.0, -0.1],
         [0.2, -1.0],
         [-0.3, 0.4],
@@ -38,14 +38,14 @@ app.create_fractal("Afractal", [
         [-0.2, 0.5]
 ], it=4*10**4, animation_need=True, multi=200)
 
-app.create_fractal("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
+app.create_figure("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
 
-app.create_fractal("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
-app.create_fractal("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
+app.create_figure("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
+app.create_figure("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
 
-app.create_fractal("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
-app.create_fractal("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
-app.create_fractal("NangularFractal", np.array([
+app.create_figure("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
+app.create_figure("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
+app.create_figure("NangularFractal", np.array([
     [0, 0],
     [1, 5],
     [2, -1],
