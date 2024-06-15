f351d8a228d8892966141ecabddea46933a2b23e
darenky <kuyesdaren@gmail.com>
2024-06-16 00:48:31 +0300

    pull from dev

--- code/Figures/LsystemFractal.py
+++ code/Figures/LsystemFractal.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LsystemFractal:
@@ -8,7 +9,7 @@ class LsystemFractal:
     Now there is no Animation and only radians
     """
 
-    def __init__(self, axiom: str, rules: dict, max_iterations: int, fi: float, dfi: float, *args):
+    def __init__(self, axiom: str, rules: dict, fi: float, dfi: float, *args):
         """
         Initiates Lsystem fractal with given parameters but before checks if parameters are correct
 
@@ -22,12 +23,11 @@ class LsystemFractal:
 
         if args != ():
             raise ValueError(f"Wrong number of arguments, {args} excess")
-        self.list_to_check = [str, dict, int, float, float]  # Change if count of arguments changes
-        self.check_args(axiom, rules, max_iterations, fi, dfi)
+        self.list_to_check = [str, dict, float, float]  # Change if count of arguments changes
+        self.check_args(axiom, rules, fi, dfi)
 
         self.axiom = axiom
         self.rules = rules
-        self.max_iterations = max_iterations
         self.fi = fi
         self.dfi = dfi
 
@@ -41,39 +41,41 @@ class LsystemFractal:
             if type(args[argument_index]) is not self.list_to_check[argument_index]:
                 raise ValueError(f"Wrong argument {args[argument_index]}, which is {type(args[argument_index])} type, expected {self.list_to_check[argument_index]} type")
 
-    def generate_points(self):
+    def generate_points(self, iteration):
         """
-        Generates more and more of Lsystem fractal on each iteration
-
-        # Updates:
-        self.axiom each iteration
-
-        Calculates all coordinates of Lsystem fractal
+        Generates specified iteration of Lsystem fractal
 
         # Returns:
         (N+1 shape, N+1 shape) arrays of x and y coordinates
         """
-        for iteration in range(self.max_iterations):
+        result = self.axiom
+        for iteration in range(iteration):
             new_axiom = ''
-            for word_place in range(len(self.axiom)):
-                if self.axiom[word_place] in self.rules.keys():
-                    new_axiom += self.rules[self.axiom[word_place]]
+            for word_place in range(len(result)):
+                if result[word_place] in self.rules.keys():
+                    new_axiom += self.rules[result[word_place]]
                 else:
-                    new_axiom += self.axiom[word_place]
-            self.axiom = new_axiom
+                    new_axiom += result[word_place]
+            result = new_axiom
 
-        N = len(self.axiom)
+        N = len(result)
         L = 2
         x = np.zeros(N+1)
         y = np.zeros(N+1)
         for i in range(N):
             x[i+1] = x[i]
             y[i+1] = y[i]
-            if self.axiom[i] == 'F':
+            if result[i] == 'F':
                 x[i+1] += L*np.cos(self.fi)
                 y[i+1] += L*np.sin(self.fi)
-            elif self.axiom[i] == '+':
+            elif result[i] == '+':
                 self.fi += self.dfi
-            elif self.axiom[i] == '-':
+            elif result[i] == '-':
                 self.fi -= self.dfi
         return x, y
+
+if __name__ == '__main__':
+    l = LsystemFractal('F', {'F': 'F+F--F+F'}, 0., 0.5)
+    x, y = l.generate_points(5)
+    plt.plot(x, y, marker='', linestyle='-', markersize=0.6)
+    plt.show()
