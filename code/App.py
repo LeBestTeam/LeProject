e10d2c02ba05cf95fb28b52f73816019f986c17f
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-15 23:30:09 +0300

    Add non fractals to app

--- code/App.py
+++ code/App.py
@@ -1,4 +1,4 @@
-from Figures import LsystemFractal, AffineFractal, MatrixFractal, JuliaSet, MandelbrotSet, BrownianTree, BrownianMotion, NangularFractal
+from Figures import LsystemFractal, AffineFractal, MatrixFractal, JuliaSet, MandelbrotSet, BrownianTree, BrownianMotion, NangularFractal, ReguralPolygon, DefaultPolynomialFunction
 from Director import FigureDirector
 
 import numpy as np
@@ -17,7 +17,10 @@ class App:
             # Random Fractals
             "BrownianTree": BrownianTree.BrownianTree,
             "BrownianMotion": BrownianMotion.BrownianMotion,
-            "NangularFractal": NangularFractal.NangularFractal
+            "NangularFractal": NangularFractal.NangularFractal,
+            # Non Fractals
+            "ReguralPolygon": ReguralPolygon.ReguralPolygon,
+            "DefaultPolynomialFunction": DefaultPolynomialFunction.DefaultPolynomialFunction,
         }
 
     def create_figure(self, name, *args, **kwargs):
@@ -28,25 +31,28 @@ class App:
 
 
 app = App()
-app.create_figure("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
-app.create_figure("Afractal", [
-        [1.0, -0.1],
-        [0.2, -1.0],
-        [-0.3, 0.4],
-        [0.7, 0.2],
-        [0.5, -0.4],
-        [-0.2, 0.5]
-], it=4*10**4, animation_need=True, multi=200)
+# app.create_figure("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
+# app.create_figure("Afractal", [
+#         [1.0, -0.1],
+#         [0.2, -1.0],
+#         [-0.3, 0.4],
+#         [0.7, 0.2],
+#         [0.5, -0.4],
+#         [-0.2, 0.5]
+# ], it=4*10**4, animation_need=True, multi=200)
 
-app.create_figure("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
+# app.create_figure("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
 
-app.create_figure("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
-app.create_figure("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
+# app.create_figure("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
+# app.create_figure("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
 
-app.create_figure("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
-app.create_figure("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
-app.create_figure("NangularFractal", np.array([
-    [0, 0],
-    [1, 5],
-    [2, -1],
-]), it=4*10**4, animation_need=True, multi=200)
+# app.create_figure("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
+# app.create_figure("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
+# app.create_figure("NangularFractal", np.array([
+#     [0, 0],
+#     [1, 5],
+#     [2, -1],
+# ]), it=4*10**4, animation_need=True, multi=200)
+
+app.create_figure("ReguralPolygon", 3, 0.5, it=100, animation_need=True, multi=200)
+app.create_figure("DefaultPolynomialFunction", 3, 0.5, [0, 0, 0, 1], it=100, animation_need=True, multi=200)
