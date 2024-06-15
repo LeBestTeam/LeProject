e9e88b96f4caf5d2d836eaf9df2e7bfe93776c98
Mykhailo Holub <mishataube2005@gmail.com>
2024-06-15 16:58:04 +0300

    Update DefaultPolynomialFunction.py

--- code/Figures/DefaultPolynomialFunction.py
+++ code/Figures/DefaultPolynomialFunction.py
@@ -2,16 +2,15 @@ import numpy as np
 
 
 class DefaultPolynomialFunction:
-    def __init__(self, start: float, stop: float, polynom: list, step: float, *args):
+    def __init__(self, start: float, stop: float, polynom: list, *args):
         if args != ():
             raise ValueError(f"Wrong number of arguments, {args} excess")
-        self.list_to_check = [float, float, list, float]  # Change if count of arguments changes
-        self.check_args(start, stop, polynom, step)
+        self.list_to_check = [float, float, list]  # Change if count of arguments changes
+        self.check_args(start, stop, polynom)
 
         self.start = start
         self.stop = stop
         self.polynom = polynom
-        self.step = step
 
     def check_args(self, *args):
         for argument_index in range(len(args)):
@@ -20,8 +19,9 @@ class DefaultPolynomialFunction:
                     f"Wrong argument {args[argument_index]}, which is {type(args[argument_index])} type, expected {self.list_to_check[argument_index]} type")
 
     def generate_points(self, iteration):
-        x = np.array(range(self.start, self.stop + self.step, self.step))
-        y = np.zeros(len(range(self.start, self.stop + self.step, self.step)))
+        step = 1/iteration
+        x = np.array(range(self.start, self.stop + step, step))
+        y = np.zeros(len(range(self.start, self.stop + step, step)))
         for i in range(len(self.polynom)):
             y += (x ** i) * self.polynom[i]
         return x, y
