02da96e434f7556fbfc11ea3695e853fb9051f20
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-15 23:31:21 +0300

    Add non fractals to app

--- code/App.py
+++ code/App.py
@@ -31,28 +31,28 @@ class App:
 
 
 app = App()
-# app.create_figure("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
-# app.create_figure("Afractal", [
-#         [1.0, -0.1],
-#         [0.2, -1.0],
-#         [-0.3, 0.4],
-#         [0.7, 0.2],
-#         [0.5, -0.4],
-#         [-0.2, 0.5]
-# ], it=4*10**4, animation_need=True, multi=200)
+app.create_figure("Lfractal", "F+F+F+F", {"F": "F+F-F-F+F"}, 0., np.pi/2, it=5, animation_need=True, multi=200)
+app.create_figure("Afractal", [
+        [1.0, -0.1],
+        [0.2, -1.0],
+        [-0.3, 0.4],
+        [0.7, 0.2],
+        [0.5, -0.4],
+        [-0.2, 0.5]
+], it=4*10**4, animation_need=True, multi=200)
 
-# app.create_figure("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
+app.create_figure("Mfractal", np.array([[0, 1, 1], [1, 0, 1], [1, 1, 0]]), it=5, is_matrix=True, animation_need=True, interval=22)
 
-# app.create_figure("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
-# app.create_figure("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
+app.create_figure("Juliafractal", complex(-0.4, 0.6), 2.0, 1000, 1000, it=100)
+app.create_figure("Mandelbrotfractal", 2.0, 1000, 1000, it=100)
 
-# app.create_figure("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
-# app.create_figure("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
-# app.create_figure("NangularFractal", np.array([
-#     [0, 0],
-#     [1, 5],
-#     [2, -1],
-# ]), it=4*10**4, animation_need=True, multi=200)
+app.create_figure("BrownianTree", False, it=30, is_matrix=True, animation_need=True)
+app.create_figure("BrownianMotion", 300, it=30000, is_matrix=True, animation_need=True)
+app.create_figure("NangularFractal", np.array([
+    [0, 0],
+    [1, 5],
+    [2, -1],
+]), it=4*10**4, animation_need=True, multi=200)
 
 app.create_figure("ReguralPolygon", 3, 0.5, it=100, animation_need=True, multi=200)
 app.create_figure("DefaultPolynomialFunction", 3, 0.5, [0, 0, 0, 1], it=100, animation_need=True, multi=200)
