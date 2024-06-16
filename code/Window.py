e3d35b08cc895cbad6637493f52dee70c4d5fc84
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-06-16 19:03:21 +0300

    Global rework

--- code/Window.py
+++ code/Window.py
@@ -2,114 +2,91 @@ import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+
 class Window:
     def draw(self, input_value, **kwargs):
-        multiplayer = 1
-        if "multiplayer" in kwargs:
-            if isinstance(kwargs["multiplayer"], (int, float)):
-                multiplayer = kwargs["multiplayer"]
-        if "multi" in kwargs:
-            if isinstance(kwargs["multi"], (int, float)):
-                multiplayer = kwargs["multi"]
+        multiplayer = kwargs.get("multiplayer", 1) * kwargs.get("multi", 1)
+        interval = kwargs.get("interval", 30)
+        markersize = kwargs.get("markersize", 0.6)
+        figsize = kwargs.get("figsize", (5, 5))
+        fps = kwargs.get("fps", 15)
 
-        interval = 1
-        if "interval" in kwargs:
-            if isinstance(kwargs["interval"], (int, float)):
-                interval = kwargs["interval"]
+        cmap = kwargs.get("cmap", 'gray')  # 'inferno'
 
-        if "animation_save" in kwargs and kwargs["animation_save"]:
+        animation_need = kwargs.get("animation_need", False)
+        animation_save = kwargs.get("animation_save", False)
+
+        is_edge = kwargs.get("is_edge", False)
+        is_fixed_size = kwargs.get("is_fixed_size", False)
+
+        has_axes = kwargs.get("has_axes", True)
+        has_background = kwargs.get("has_background", True)
+
+        if animation_save:
             if not os.path.isdir("images"):
                 os.mkdir("images")
             add_to_name = len(os.listdir("./images"))
+            filename = kwargs.get('filename', 'figure' + str(add_to_name))
 
-        if "is_matrix" in kwargs and kwargs["is_matrix"]:
-            if isinstance(input_value, list):
-                if "animation_need" in kwargs and kwargs["animation_need"]:
-                    fig, ax = plt.subplots()
-
-                    ax.imshow(input_value[0], cmap='gray')  # matrix
-
-                    def update(frame):
-                        ax.clear()
-                        ax.imshow(input_value[frame], cmap='gray')
-
-                    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(input_value), interval=30 * interval)
-                    if "animation_save" in kwargs and kwargs["animation_save"]:
-                        ani.save("./images/figure_" + str(add_to_name) + '.gif', writer=animation.PillowWriter(fps=15))
-                else:
-                    plt.imshow(input_value[-1], cmap='gray')
-            else:
-                plt.imshow(input_value, cmap='gray')
-
-        elif isinstance(input_value, tuple):
+        try:
             x, y = input_value
-            if "is_edge" in kwargs and kwargs["is_edge"]:
+            is_matrix = False
+        except ValueError:
+            is_matrix = True
+
+        if is_matrix:
+            if not isinstance(input_value, list):
+                plt.imshow(input_value, cmap=cmap)
+            elif not animation_need:
+                plt.imshow(input_value[-1], cmap=cmap)
+            else:
+                fig, ax = plt.subplots(figsize=figsize, constrained_layout=(not has_background))
+
+                ax.imshow(input_value[0], cmap=cmap)
+                if not has_axes:
+                    ax.axis('off')
+
+                def update(frame):
+                    ax.clear()
+                    ax.imshow(input_value[frame], cmap=cmap)
+                    if not has_axes:
+                        ax.axis('off')
+
+                ani = animation.FuncAnimation(fig=fig, func=update, frames=len(input_value), interval=interval)
+        else:
+            if is_fixed_size:
+                x_limit_left = min(x) - abs(min(x)) / 2
+                x_limit_right = max(x) + abs(max(x)) / 2
+                y_limit_bottom = min(y) - abs(min(y)) / 2
+                y_limit_top = max(y) + abs(max(y)) / 2
+            if is_edge:
                 plt.plot(x, y)
             else:
-                markersize = kwargs.get("markersize", 0.6)
-                if "animation_need" in kwargs and kwargs["animation_need"]:
-                    fig, ax = plt.subplots()
+                if not animation_need:
+                    plt.plot(x, y, marker='o', linestyle='', markersize=markersize)
+                else:
+                    fig, ax = plt.subplots(figsize=figsize, constrained_layout=(not has_background))
 
+                    if is_fixed_size:
+                        ax.set_xlim(x_limit_left, x_limit_right)
+                        ax.set_ylim(y_limit_bottom, y_limit_top)
+                    if not has_axes:
+                        ax.axis('off')
                     ax.plot(x, y, marker='o', linestyle='', markersize=markersize)
 
                     def update(frame):
                         frame = frame * multiplayer
                         ax.clear()
+                        if is_fixed_size:
+                            ax.set_xlim(x_limit_left, x_limit_right)
+                            ax.set_ylim(y_limit_bottom, y_limit_top)
+                        if not has_axes:
+                            ax.axis('off')
                         ax.plot(x[:frame], y[:frame], marker='o', linestyle='', markersize=markersize)
 
-                    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(x) // multiplayer, interval=30 * interval)
-                    if "animation_save" in kwargs and kwargs["animation_save"]:
-                        ani.save("./images/figure_" + str(add_to_name) + '.gif', writer=animation.PillowWriter(fps=15))
-                else:
-                    plt.plot(x, y, marker='o', linestyle='', markersize=markersize)
+                    ani = animation.FuncAnimation(fig=fig, func=update, frames=len(x)//multiplayer, interval=interval)
 
-        else:  
-            fig, ax = plt.subplots(figsize=(8, 8))
-
-            cmap = plt.get_cmap('inferno')
-
-            frames = []
-
-            max_iter = kwargs.get('max_iter', 100)
-
-            for i in range(max_iter):
-                frame = ax.imshow(input_value.T, cmap=cmap, vmin=0, vmax=max_iter)
-                frame.set_clim(vmin=0, vmax=i)
-                frames.append([frame])
-
-            ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True, repeat_delay=1000)
-
-            if kwargs.get('save_gif', False):
-                filename = kwargs.get('filename', 'morphing.gif') # it saves fractal with its own name!! 
-                filepath = os.path.join('./images', filename)
-                writer = animation.PillowWriter(fps=30)
-                ani.save(filepath, writer=writer)
-            
-            else:
-                plt.show()
-
+        if animation_need and animation_save:
+            ani.save("./images/" + filename + '.gif', writer=animation.PillowWriter(fps=fps))
 
         plt.show()
-
-
-
-''' my initial draw() for morhing fractals only (in case i incorectly merged it into Maks's code for draw()'''
-    # def draw(self, iterations, max_iter=100, save_gif=False, filename='julia_set.gif'):
-    #         fig, ax = plt.subplots(figsize=(8, 8))
-
-    #         cmap = plt.get_cmap('inferno')
-
-    #         frames = []
-
-    #         for i in range(max_iter):
-    #             frame = ax.imshow(iterations.T, cmap=cmap, vmin=0, vmax=max_iter)
-    #             frame.set_clim(vmin=0, vmax=i)
-    #             frames.append([frame])
-
-    #         ani = animation.ArtistAnimation(fig, frames, interval=50, blit=True, repeat_delay=1000)
-
-    #         if save_gif:
-    #             writer = animation.PillowWriter(fps=30)
-    #             ani.save(filename, writer=writer)
-    #         else:
-    #             plt.show()
